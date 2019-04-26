899c9669074a1a23457e4143e96756984bd90a6b
behzad88 <behzad88.2012@outlook.com>
2019-04-27 00:35:08 +0430

    chmod

--- scripts/circleci/set_up_github_keys.sh
+++ scripts/circleci/set_up_github_keys.sh
@@ -3,8 +3,7 @@
 set -e
 
 if [ -n "$GITHUB_TOKEN" ]; then
-
-  GH_PAGES_DIR=$(pwd)/../reactx-gh-pages
+  
   echo "machine github.com login behzad888 password $GITHUB_TOKEN" >~/.netrc
   git config --global user.name "Circle CI"
   git config --global user.email "behzad88.2012@gmail.com"
