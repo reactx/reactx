ba3ba03e2fed0f66cd60b5b57aba4efd66a66fd7
behzad88 <behzad88.2012@outlook.com>
2019-04-27 01:22:10 +0430

    build force

--- scripts/circleci/set_up_github_keys.sh
+++ scripts/circleci/set_up_github_keys.sh
@@ -3,9 +3,7 @@
 set -e
 
 if [ -n "$GITHUB_TOKEN" ]; then
-  
   echo "machine github.com login behzad888 password $GITHUB_TOKEN" >~/.netrc
   git config --global user.name "Behzad Abbasi"
   git config --global user.email "behzad88.2012@gmail.com"
-
 fi
