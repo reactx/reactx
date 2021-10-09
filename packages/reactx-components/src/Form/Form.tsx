14c786fedfe7f3c87ec7df76db10e3b7438f1eb5
behzad888 <behzad88.2012@gmail.com>
2021-10-10 01:05:52 +0330

    prevent form

--- packages/reactx-components/src/Form/Form.tsx
+++ packages/reactx-components/src/Form/Form.tsx
@@ -8,26 +8,44 @@
  */
 
 import classNames from 'classnames';
-import React, {ForwardedRef} from 'react';
+import React, {ForwardedRef, useCallback} from 'react';
+import '../assets/elements.form.scss';
 import Check from './Check';
 import Control from './Control';
 import DateTime from './DateTime';
 import Select from './Select';
 import Switch from './Switch';
-import '../assets/elements.form.scss';
 import TextArea from './TextArea';
 
 export interface FormPropsType
   extends React.FormHTMLAttributes<HTMLFormElement> {
   forawardedRef?: ForwardedRef<HTMLFormElement>;
   validated?: boolean;
+  preventDfaultAction?: boolean;
 }
 
 const FormComponent = (props: FormPropsType) => {
-  const {forawardedRef, className, validated, ...restProps} = props;
+  const {
+    forawardedRef,
+    className,
+    validated,
+    preventDfaultAction,
+    onSubmit,
+    ...restProps
+  } = props;
+
+  const handleSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      // prevent the default form action.
+      if (preventDfaultAction) e.preventDefault();
+      if (onSubmit) onSubmit(e);
+    },
+    [preventDfaultAction, onSubmit],
+  );
   return (
     <form
       ref={forawardedRef}
+      onSubmit={handleSubmit}
       className={classNames('x-form', className, {
         'x-form--validate': validated,
       })}
@@ -41,6 +59,7 @@ const Form = React.forwardRef<HTMLFormElement, FormPropsType>((props, ref) => (
 
 Form.defaultProps = {
   validated: true,
+  preventDfaultAction: false,
 };
 Form.displayName = 'Form';
 export default Object.assign(Form, {
