db3cd798b6964f6f22df8f7318377136bc5df5fb
Parsa Khosravani <72360929+parsakhosravani@users.noreply.github.com>
2022-12-26 15:24:08 +0330

    add table foot

---
+++ packages/reactx-web-components/src/Table/TFoot.tsx
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) ReactX and its affiliates..
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import React from "react";
+import clsx from "clsx";
+
+export interface TFootPropsType extends React.TableHTMLAttributes<HTMLTableCaptionElement> {
+
+}
+
+const TFoot: React.FC<TFootPropsType> = (props) => {
+  const {children, className, ...restProps} = props;
+  return (
+    <tfoot className={clsx('x-tfoot', className)} {...restProps}>
+    {children}
+    </tfoot>
+  )
+}
+
+export default TFoot;
